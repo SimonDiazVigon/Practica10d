0e9cb9a01ed3397edc1e8b5a7aead01d77a1e897
SimonDiazVigon <simondivi@gmail.com>
2019-05-28 01:14:56 +0200

    assfv

--- src/main/java/asr/proyectoFinal/services/Traductor.java
+++ src/main/java/asr/proyectoFinal/services/Traductor.java
@@ -2,36 +2,49 @@ package asr.proyectoFinal.services;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import
-com.ibm.watson.developer_cloud.language_translator.v2.LanguageTranslator;
-import
-com.ibm.watson.developer_cloud.language_translator.v2.util.Language;
-import
-com.ibm.watson.developer_cloud.language_translator.v2.model.TranslateOptions.Builder;
-import
-com.ibm.watson.developer_cloud.language_translator.v2.model.TranslateOptions;
-import
-com.ibm.watson.developer_cloud.language_translator.v2.model.TranslationResult;
+import com.ibm.watson.developer_cloud.language_translator.v3.LanguageTranslator;
+import com.ibm.watson.developer_cloud.language_translator.v3.model.TranslateOptions;
+import com.ibm.watson.developer_cloud.language_translator.v3.model.TranslationResult;
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+
 public class Traductor
 {
-public static String translate(String palabra)
-{
-LanguageTranslator service = new LanguageTranslator();
-service.setUsernameAndPassword("USER NAME", "PASSWORD");
-TranslateOptions translateOptions = new TranslateOptions.Builder()
- .addText(palabra)
- .modelId("en-es")
- .build();
-TranslationResult translationResult = service.translate(translateOptions).execute();
-System.out.println(translationResult);
-String traduccionJSON = translationResult.toString();
-JsonParser parser = new JsonParser();
-JsonObject rootObj = parser.parse(traduccionJSON).getAsJsonObject();
-String wordCount = rootObj.get("word_count").getAsString();
-JsonArray traducciones = rootObj.getAsJsonArray("translations");
-String traduccionPrimera = palabra;
-if(traducciones.size()>0)
-traduccionPrimera = traducciones.get(0).getAsJsonObject().get("translation").getAsString();
-return traduccionPrimera;
-}
+	public static String translate(String palabra, String sourceModel, String destModel, boolean conversational)
+	{
+		String model;
+		if(sourceModel.equals("en") || destModel.equals("en"))
+		{
+			model=sourceModel+"-"+destModel;
+			if(conversational) 
+				model+="-conversational";
+		}
+		else
+			return translate(translate(palabra,sourceModel,"en",conversational),"en",destModel,conversational); //translate to english, then to dest
+
+		LanguageTranslator languageTranslator = new LanguageTranslator("2019-04-09");
+		languageTranslator.setUsernameAndPassword("3b660dde-c20d-4726-8a7f-7402cab1314e","IC2_s8UuNWiGjqbVbEx4Gp3hwMBgZ-1rViKTD8nnBX3S");
+
+		languageTranslator.setEndPoint("https://gateway.watsonplatform.net/language-translator/api");
+		
+		TranslateOptions translateOptions = new
+		TranslateOptions.Builder()
+		 .addText(palabra)
+		 .modelId(model)
+		 .build();
+
+		TranslationResult translationResult = languageTranslator.translate(translateOptions).execute();
+		
+		System.out.println(translationResult);
+
+		String traduccionJSON = translationResult.toString();
+		JsonParser parser = new JsonParser();
+		JsonObject rootObj =
+		parser.parse(traduccionJSON).getAsJsonObject();
+		JsonArray traducciones = rootObj.getAsJsonArray("translations");
+		String traduccionPrimera = palabra;
+		if(traducciones.size()>0)
+		traduccionPrimera =
+		traducciones.get(0).getAsJsonObject().get("translation").getAsString();
+		return traduccionPrimera;
+	}
 }
